37dc252c1f2ea895999f8237c77a09bc3ad69bf9
Kyle de Vos <kdevos827@gmail.com>
2022-09-26 10:34:56 +0200

    Completed Lab - Spring Fundamanetals - success page

---
+++ demo/src/main/java/com/example/demo/StatusService.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatusService {
+
+    public String processStatus(boolean arriving) {
+        if (arriving) {
+            return "hello";
+        } else {
+            return "goodbye";
+        }
+    }
+
+    public String displaySuccess(){
+        return "success";
+    }
+}
