63101ecd88c10d96445c486f88a708c4e10d2bfc
Kyle de Vos <kdevos827@gmail.com>
2022-09-29 23:38:59 +0200

    Completed Constructor Injection Example

--- src/main/java/platform/codingnomads/co/ioc/examples/constructorinjection/Laptop.java
+++ src/main/java/platform/codingnomads/co/ioc/examples/constructorinjection/Laptop.java
@@ -9,17 +9,20 @@ public class Laptop {
 
     private Processor processor;
     private OS os;
+    private Battery battery;
 
     // these fields are being injected from the ApplicationContext
     // if a class contains only one constructor (as this one does), the use of @Autowired is optional
     // if a class contains two or more constructors, @Autowired is required for constructor injection to take place
-    public Laptop(Processor processor, OS os) {
+    public Laptop(Processor processor, OS os, Battery battery) {
         this.processor = processor;
         this.os = os;
+        this.battery = battery;
     }
 
     public String printLaptopConfiguration() {
         return "processor: " + processor.getCore() + " core " + processor.getName() +
-                "\nOS: " + os.getName();
+                "\nOS: " + os.getName()+
+                "\nBattery Level: " + battery.getBatteryLevel();
     }
 }
