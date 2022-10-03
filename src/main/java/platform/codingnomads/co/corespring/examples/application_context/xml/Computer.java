b06107f56573c300982019781707abcc12ce4698
Kyle de Vos <kdevos827@gmail.com>
2022-10-03 13:05:38 +0200

    Completed XML Configuration Lab - Spring Core

---
+++ src/main/java/platform/codingnomads/co/corespring/examples/application_context/xml/Computer.java
@@ -0,0 +1,14 @@
+package platform.codingnomads.co.corespring.examples.application_context.xml;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Computer {
+
+    private String brand;
+    private String specifications;
+}
