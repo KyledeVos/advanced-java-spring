7636aa3e2d84fbc1bda73dcc87ca443e8ce4e6a0
Kyle de Vos <kdevos827@gmail.com>
2022-10-13 22:22:07 +0200

    Completed CoreSpring componentScan Task

--- src/main/java/platform/codingnomads/co/corespring/examples/componentscanannotation/ComponentScanDemo.java
+++ src/main/java/platform/codingnomads/co/corespring/examples/componentscanannotation/ComponentScanDemo.java
@@ -9,7 +9,7 @@ public class ComponentScanDemo {
 
     public static void main(String[] args) {
         applicationContext = new AnnotationConfigApplicationContext(ComponentScanConfiguration.class);
-        isBeanPresent("framework", "JDK", "motherBoard", "OS", "sampleBean","componentScanConfiguration");
+        isBeanPresent("framework", "JDK", "motherBoard", "OS", "sampleBean","componentScanConfiguration", "speaker", "mouse", "flashDrive");
     }
 
     private static void isBeanPresent(String... beans) {
---
+++ src/main/java/platform/codingnomads/co/corespring/examples/componentscanannotation/external/Mouse.java
@@ -0,0 +1,7 @@
+package platform.codingnomads.co.corespring.examples.componentscanannotation.external;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mouse {
+}
