2a26e950ebc6221a088e598aecf2dd14b34006f1
KyledeVos <kdevos827@gmail.com>
2023-01-11 16:43:43 +0200

    Completed IntroducingRepositories CrudRepository Task

--- src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/crudrepository/CrudRepoDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/introducingrepositories/crudrepository/CrudRepoDemo.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootApplication
 public class CrudRepoDemo implements CommandLineRunner {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CrudRepoDemo.class);
     }
 
@@ -26,18 +28,31 @@ public class CrudRepoDemo implements CommandLineRunner {
         User user = User.builder().firstName("Bobby").lastName("Bobbert").age(56).build();
         User user2 = User.builder().firstName("Joanne").lastName("Joanna").age(36).build();
 
-        //save user and assign what is returned to the user variable.
-        user = userRepo.save(user);
-        user2 = userRepo.save(user2);
+        //task
+        User user3 = User.builder().firstName("Harry").lastName("Miles").age(38).build();
+        User user4 = User.builder().firstName("Sarah").lastName("Clair").age(29).build();
+        User user5 = User.builder().firstName("Nelly").lastName("Jackson").age(45).build();
+        User user6 = User.builder().firstName("Amanda").lastName("Hudge").age(78).build();
 
-        Iterable<User> users = userRepo.findAll();
+        //save user and assign what is returned to the user variable.
+//        user = userRepo.save(user);
+//        user2 = userRepo.save(user2);
+
+        //task
+        Iterable<User> users = userRepo.saveAll(List.of(user, user2, user3, user4, user5, user6));
+
+        //findAll users from database
+        users = userRepo.findAll();
 
         for(User u : users){
             System.out.println(u.toString());
         }
 
         //delete the user using the id of the inserted user object
-        userRepo.deleteById(user.getId());
-        userRepo.deleteById(user2.getId());
+//        userRepo.deleteById(user.getId());
+//        userRepo.deleteById(user2.getId());
+
+        //delete all users at once from database
+        userRepo.deleteAll(users);
     }
 }
